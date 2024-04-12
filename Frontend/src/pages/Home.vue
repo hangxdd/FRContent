b2e1d1663b396e1c4a40e16d568de769f119f808
hangxdd <mrlivescreamlv@gmail.com>
2024-04-12 17:13:18 +0200

    Fixed no data styling and added FRContent logo to reset pass page

--- Frontend/src/pages/Home.vue
+++ Frontend/src/pages/Home.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="mt-10 py-6 flex flex-col justify-center sm:py-12">
+    <div v-if="!useAuth.user" class="flex justify-center items-center h-screen">
+        <h3 class="text-3xl text-red-500 font-bold">No data</h3>
+    </div>
+
+    <div v-if="useAuth.user" class="mt-10 py-6 flex flex-col justify-center sm:py-12">
         <div class="relative py-3 sm:max-w-xl sm:mx-auto">
             <div
                 class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
             </div>
             <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-                <div v-if="useAuth.user">
+                <div>
                     <h2 class="text-2xl font-bold text-center">Welcome, {{ useAuth.user.name }}!</h2>
                     <p class="text-center mt-4">Discover movies tailored to your emotions.</p>
                     <router-link to="/scan"
@@ -14,11 +18,7 @@
                     </router-link>
                 </div>
 
-                <div v-else class="flex justify-center">
-                    <h3 class="text-3xl font-bold">No data</h3>
-                </div>
-
-                <div class="mt-10">
+                <div v-if="useAuth.user" class="mt-10">
                     <h2 class="text-2xl font-bold text-center">How It Works</h2>
                     <p class="text-center mt-4">Our facial recognition technology analyzes your emotions and recommends
                         movies that match.</p>
