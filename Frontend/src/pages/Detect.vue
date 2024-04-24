ef760d3cc07857d10ee74f484e50260e9789e076
hangxdd <mrlivescreamlv@gmail.com>
2024-04-24 19:58:32 +0300

    Added "New recommendations" button
    - Added button in the "Recommendations" tab "New recommendations" that allows the user to generate new movie recommendations based on it's last detected emotion.

--- Frontend/src/pages/Detect.vue
+++ Frontend/src/pages/Detect.vue
@@ -76,6 +76,26 @@
               <p class="text-gray-600 mb-4">
                 Based on the emotion: <strong>{{ emotionValue }}</strong>
               </p>
+              <button
+                class="w-full bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mb-4 flex justify-center items-center"
+                @click="captureEmotion"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6 mr-2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                  />
+                </svg>
+                New recommendations
+              </button>
               <div class="grid grid-cols-1 gap-4">
                 <div
                   v-for="(movie, index) in recommendedMovies"
