ebda6daa4ea12fd58bde1bace746d2cec2b86887
hangxdd <mrlivescreamlv@gmail.com>
2024-05-06 15:13:31 +0300

    Added "Favourite" button to movies in the "Recommendations" tab

--- Frontend/src/pages/Detect.vue
+++ Frontend/src/pages/Detect.vue
@@ -246,6 +246,31 @@
                         No providers found...
                       </p>
                     </div>
+                    <button
+                      class="w-full text-white font-bold p-2 mt-4 rounded flex justify-center items-center"
+                      :class="
+                        isFavourited[movie.id]
+                          ? 'bg-blue-600 hover:bg-blue-500'
+                          : 'bg-blue-300 hover:bg-blue-200'
+                      "
+                      @click="toggleFavouriteMovie(movie.id)"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        :fill="isFavourited[movie.id] ? 'white' : 'none'"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6 mr-1"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                        />
+                      </svg>
+                      {{ isFavourited[movie.id] ? "Unfavourite" : "Favourite" }}
+                    </button>
                   </div>
                 </div>
               </div>
