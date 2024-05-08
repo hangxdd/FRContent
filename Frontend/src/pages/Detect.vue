c1222aef7b100eba74935f20c99802bbf8ac3f90
hangxdd <mrlivescreamlv@gmail.com>
2024-05-08 18:42:55 +0300

    Added option to sort by title

--- Frontend/src/pages/Detect.vue
+++ Frontend/src/pages/Detect.vue
@@ -513,6 +513,32 @@
                     <div class="py-1">
                       <MenuItem v-slot="{ active }">
                         <a
+                          @click="() => sortByTitle('History')"
+                          :class="[
+                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                            'w-full inline-flex px-4 py-2 text-sm items-center cursor-pointer',
+                          ]"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6 mr-2"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                            />
+                          </svg>
+
+                          Title
+                        </a>
+                      </MenuItem>
+                      <MenuItem v-slot="{ active }">
+                        <a
                           @click="() => sortByGenre('History')"
                           :class="[
                             active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
@@ -995,6 +1021,32 @@
                       <div class="py-1">
                         <MenuItem v-slot="{ active }">
                           <a
+                            @click="() => sortByTitle('Favourites')"
+                            :class="[
+                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'w-full inline-flex px-4 py-2 text-sm items-center cursor-pointer',
+                            ]"
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke-width="1.5"
+                              stroke="currentColor"
+                              class="w-6 h-6 mr-2"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                              />
+                            </svg>
+
+                            Title
+                          </a>
+                        </MenuItem>
+                        <MenuItem v-slot="{ active }">
+                          <a
                             @click="() => sortByGenre('Favourites')"
                             :class="[
                               active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
@@ -1473,6 +1525,17 @@ watchEffect(() => {
     toggleVideo();
   }
 });
+
+const sortByTitle = (category) => {
+  if (categories.value[category] instanceof Array) {
+    const sortedArray = [...categories.value[category]].sort((a, b) => {
+      if (a.title > b.title) return 1;
+      if (a.title < b.title) return -1;
+      return 0;
+    });
+    categories.value[category] = sortedArray;
+  }
+};
 
 const sortByGenre = (category) => {
   if (categories.value[category] instanceof Array) {
